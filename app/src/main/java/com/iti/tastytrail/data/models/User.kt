4b090c1783f0f39714a78294c3cb5382c8436b69
Hekal <ahmedsanta021@gmail.com>
2025-09-05 18:42:02 +0300

    change id to be auto genrated

--- app/src/main/java/com/iti/tastytrail/data/models/User.kt
+++ app/src/main/java/com/iti/tastytrail/data/models/User.kt
@@ -5,8 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class User(
-    @PrimaryKey
-    val id: String,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
     val username: String,
     val email: String,
     val password: String
